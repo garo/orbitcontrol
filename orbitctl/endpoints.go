842774220ee42d9a799a7a0ef4c9952e49da4cbc
Juho Mäkinen <juho.makinen@applifier.com>
2014-11-21 13:13:35 +0200

    Pipe friendly output options

--- orbitctl/endpoints.go
+++ orbitctl/endpoints.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"os"
+	"sort"
+	"strings"
 )
 
 func init() {
@@ -12,7 +14,17 @@ func init() {
 			Name:  "endpoints",
 			Usage: "Gets list of endpoints for a single service or summary for all endpoints",
 			Action: func(c *cli.Context) {
-				runEndpoints(c.Args())
+				fields := []string{}
+				if c.String("fields") != "" {
+					fields = strings.Split(c.String("fields"), ",")
+				}
+				runEndpoints(c.Args(), fields)
+			},
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "fields",
+					Usage: "List of fields to print separated by comma. Usable for using output for scripting. Possibilites: [ip,endpoint,revision,port]",
+				},
 			},
 			BashComplete: func(c *cli.Context) {
 				services, err := containrunnerInstance.GetAllServices(nil)
@@ -32,7 +44,7 @@ func init() {
 		})
 }
 
-func runEndpoints(args []string) (exit int) {
+func runEndpoints(args []string, fields []string) (exit int) {
 
 	if len(args) == 0 {
 		services, err := containrunnerInstance.GetAllServices(nil)
@@ -41,7 +53,14 @@ func runEndpoints(args []string) (exit int) {
 			return 1
 		}
 
+		keys := []string{}
 		for service_name, _ := range services {
+			keys = append(keys, service_name)
+		}
+
+		sort.Strings(keys)
+
+		for _, service_name := range keys {
 			endpoints, err := containrunnerInstance.GetEndpointsForService(service_name)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error: %+v\n", err)
@@ -50,7 +69,6 @@ func runEndpoints(args []string) (exit int) {
 
 			fmt.Printf("service %s has %d endpoints\n", service_name, len(endpoints))
 		}
-
 	}
 
 	if len(args) == 1 {
@@ -64,15 +82,36 @@ func runEndpoints(args []string) (exit int) {
 		}
 
 		for endpoint, endpointInfo := range endpoints {
-			if endpointInfo != nil && endpointInfo.Revision != "" {
-				fmt.Printf("service %s endpoint at %-22s running revision %s\n", service_name, endpoint, endpointInfo.Revision)
+			if len(fields) > 0 {
+				var str = ""
+				for _, field := range fields {
+					if field == "ip" {
+						str = fmt.Sprintf("%s%s ", str, endpoint[0:strings.Index(endpoint, ":")])
+					} else if field == "port" {
+						str = fmt.Sprintf("%s%s ", str, endpoint[strings.Index(endpoint, ":")+1:])
+					} else if field == "endpoint" {
+						str = fmt.Sprintf("%s%s ", str, endpoint)
+					} else if field == "revision" {
+						str = fmt.Sprintf("%s%s ", str, endpointInfo.Revision)
+					}
+				}
+				if len(str) > 0 {
+					str = str[0 : len(str)-1] // Strip tailing white space
+					fmt.Printf("%s\n", str)
+				}
 			} else {
-				fmt.Printf("service %s endpoint at %s\n", service_name, endpoint)
+				if endpointInfo != nil && endpointInfo.Revision != "" {
+					fmt.Printf("service %s endpoint at %-22s running revision %s\n", service_name, endpoint, endpointInfo.Revision)
+				} else {
+					fmt.Printf("service %s endpoint at %s\n", service_name, endpoint)
+				}
 			}
 		}
 
 		if len(endpoints) == 0 {
-			fmt.Fprintf(os.Stderr, "No endpoints running for service %s!\n", service_name)
+			if len(fields) == 0 {
+				fmt.Fprintf(os.Stderr, "No endpoints running for service %s!\n", service_name)
+			}
 			return 1
 		}
 
